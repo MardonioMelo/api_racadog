826d531ac151daf47ccf0ceb456f20251aef8d56
MardonioMelo <mardonio.quimico@gmail.com>
2021-01-19 23:11:10 -0300

    Update

--- public/index.php
+++ public/index.php
@@ -6,11 +6,29 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+
 $app = AppFactory::create();
+$app->setBasePath("/api-raca-dog");
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Olá Mundo!");
     return $response;
 });
 
-$app->run();
+$app->get('/lista-racas', function (Request $request, Response $response, array $args) {
+    $payload = json_encode(['ola' => 'mundo'], JSON_PRETTY_PRINT);
+
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+
+
+//Desativar erros em produção
+//$app->addErrorMiddleware(false, true, true);
+
+try {
+    $app->run();
+} catch (Exception $e) {
+    die(json_encode(array("status" => "failed", "message" => "Esta ação não é permitida!")));
+}
